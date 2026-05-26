70be368a8cb081ca772f9fc00b241ce4a77f62bd
Gary Barnes <gary.m.barnes44@outlook.com>
2026-05-26 01:52:16 -0400

    update

---
+++ DotnetSelenium/SeleniumCustomMethods.cs
@@ -0,0 +1,36 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotnetSelenium
+{
+    public class SeleniumCustomMethods
+    {
+        public static void Click(IWebDriver driver, By locator)
+
+        {
+            driver.FindElement(locator).Click();
+
+        }
+
+        public static void EnterText(IWebDriver driver, By locator, string text)
+
+        {
+            driver.FindElement(locator).Clear();
+            driver.FindElement(locator).SendKeys(text);
+
+        }
+
+        public static void Submit(IWebDriver driver, By locator)
+
+        {
+            driver.FindElement(locator).Submit();
+            
+
+        }
+
+        
+
+    }
+}
--- DotnetSelenium/UnitTest1.cs
+++ DotnetSelenium/UnitTest1.cs
@@ -14,9 +14,19 @@ namespace DotnetSelenium
         public void Test1()
         {
             IWebDriver driver = new ChromeDriver();
-            driver.Navigate().GoToUrl("https://www.google.com");
+            driver.Navigate().GoToUrl("https://www.amazon.com");
             driver.Manage().Window.Maximize();
-            IWebElement searchBox = driver.FindElement(By.Name("q"));
+            //IWebElement searchBox = driver.FindElement(By.Name("q"));
+            SeleniumCustomMethods.Click(driver, By.Id("twotabsearchtextbox")); 
+            //searchBox.SendKeys("selenium");
+            SeleniumCustomMethods.EnterText(driver, By.Id("twotabsearchtextbox"), "selenium");
+            //searchBox.SendKeys(Keys.Return);
+            SeleniumCustomMethods.Submit(driver, By.Id("twotabsearchtextbox"));
+
+            //IWebElement LoginLink = driver.FindElement(By.LinkText("Gmail"));
+            //LoginLink.Click();
+
+            
         }
     }
 }
